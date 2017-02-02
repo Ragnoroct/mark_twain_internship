2dceb8fa287740408fcffce03fb6c83f307857b3
Will Bender <willypbender83@gmail.com>
2017-02-02 17:16:13 -0600

    Add db path conts

--- consts.py
+++ consts.py
@@ -1,3 +1,13 @@
+import os
+
+_DB_NAME = "db.json"
+_ROOT_DB_NAME = "Mark_Twain_Database/"
+_APP_DATA_DIR = os.environ['LOCALAPPDATA'].replace('\\', '/') + "/"
+_ROOT_DB_DIR = _APP_DATA_DIR + _ROOT_DB_NAME
+BOOKS_DIR = _ROOT_DB_DIR + "books/"
+DB_PATH = _ROOT_DB_DIR + _DB_NAME
+
+
 TITLE = "title"
 CREATOR = "creator"
 DATE = "date"
